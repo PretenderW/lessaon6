8f2f157f3642a0fa1c541e9b88498eab324ebbef
Matsan Yevhenii <matsan_yevhenii@yahoo.com>
2018-12-06 09:54:48 +0200

    Work MatsanE  Changes to be committed: 	modified:   car.js 	modified:   index.js 	modified:   model.js 	modified:   user.js

--- car.js
+++ car.js
@@ -6,11 +6,30 @@ class Car extends Model {
   }
 
   constructor() {
+    super();
     this.pk = 'id';
     this.fields = ['id', 'user_id', 'model', 'year'];
 
-    super();
+    this.toString = function(){
+       let str = `Car pk : ${this.pk}\n\t`;
+       str += `fields : [`;
+       const n =  this.fields.length;
+       for (let i = 0;  i < n; i++){
+         if (i != 0)
+           str +=', '
+          str += `${this.fields[i]}`;
+        }
+        str+=`]`;
+       return str;
+    };
   }
+
+  setData(id, model, year){
+    this.fields = ['id', id, model, year];
+  }
+
+
+
 }
 
-module.exports = User;
+module.exports = Car;
